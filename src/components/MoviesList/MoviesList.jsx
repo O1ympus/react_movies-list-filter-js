79fe0d2f6a26d7bc4da7188e8e54e6d5b4009258
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-20 21:50:35 +0300

    sol

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,9 +1,17 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
+function filterMovie(query, arr) {
+  return arr.filter(
+    movie =>
+      movie.title.toLowerCase().includes(query.trim().toLowerCase()) ||
+      movie.description.toLowerCase().includes(query.trim().toLowerCase()),
+  );
+}
+
+export const MoviesList = ({ query, movies }) => (
   <div className="movies">
-    {movies.map(movie => (
+    {filterMovie(query, movies).map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
